bf18bfd939aa3498831370b96fac4858bcf7ad6c
Ech0ke <aidas.zygas@gmail.com>
2023-07-26 18:55:34 +0300

    Displayed user's email to user in order to test possibility to extract user info from token

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,40 @@
-import React from "react";
-import Logo from "@/images/demoLogo.svg";
-import Image from "next/image";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import Nav from "@/components/Nav";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+
+type userInfoProps = {
+  message: string;
+  success: boolean;
+  data: {
+    _id: string;
+    username: string;
+    email: string;
+    isVerified: boolean;
+  };
+};
 
 function ProfilePage() {
+  const [userData, setUserData] = useState<userInfoProps | null>(null);
+
+  useEffect(() => {
+    async function getUserInfo(): Promise<void> {
+      try {
+        const response = await axios.get("/api/users/me");
+        if (response !== null) {
+          setUserData(response.data);
+        } else {
+          toast.error("Failed to fetch user info");
+        }
+      } catch (error) {
+        console.error("Error fetching user information:", error);
+      }
+    }
+    getUserInfo();
+  }, []);
+
   return (
     <>
       <div className="min-h-screen">
@@ -11,6 +42,7 @@ function ProfilePage() {
         <h1>Profile</h1>
 
         <p>Profile page</p>
+        <span>{userData?.data?.email}</span>
       </div>
     </>
   );
