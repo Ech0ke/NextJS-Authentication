e32fd33c8139e55d5dc0629ad46f550bdf334943
Ech0ke <aidas.zygas@gmail.com>
2023-07-27 13:04:45 +0300

    Impoved ux in /profile and /profile/:id

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,16 +1,19 @@
+import Nav from "@/components/Nav";
 import React from "react";
 
 function UserProfile({ params }: any) {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen py-2">
-      <h1>Profile</h1>
-
-      <p className="text-4xl">
-        Profile page{" "}
-        <span className="p-2 rounded bg-orange-500 text-black">
-          {params.id}
-        </span>
-      </p>
+    <div>
+      <Nav />
+      <div className="flex flex-col justify-center space-y-4 items-center min-h-screen">
+        <h1>Profile</h1>
+        <div className="flex flex-wrap justify-center gap-2">
+          <p className="text-2xl ">Profile page </p>
+          <span className="p-2 rounded bg-orange-500 text-black">
+            {params.id}
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,6 +4,8 @@ import React, { useEffect, useState } from "react";
 import Nav from "@/components/Nav";
 import axios from "axios";
 import { toast } from "react-hot-toast";
+import Link from "next/link";
+import { BeatLoader } from "react-spinners";
 
 type userInfoProps = {
   message: string;
@@ -34,15 +36,32 @@ function ProfilePage() {
     }
     getUserInfo();
   }, []);
-
   return (
     <>
       <div className="min-h-screen">
         <Nav />
-        <h1>Profile</h1>
+        {!userData ? (
+          <div className="flex flex-col gap-2 justify-center items-center min-h-[85vh]">
+            <BeatLoader color="white" size={18} speedMultiplier={0.9} />
 
-        <p>Profile page</p>
-        <span>{userData?.data?.email}</span>
+            <p className="text-gray-400">loading</p>
+          </div>
+        ) : (
+          <>
+            <h1>Profile</h1>
+
+            <p>Profile page</p>
+            <p>
+              User email:{" "}
+              <Link
+                href={`/profile/${userData?.data?._id}`}
+                className="text-orange-600 underline"
+              >
+                {userData?.data?.email}
+              </Link>
+            </p>
+          </>
+        )}
       </div>
     </>
   );
