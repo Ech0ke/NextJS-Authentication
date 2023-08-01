1022e4b7e920539858a7bb2db62824ac3e32c4a8
Ech0ke <aidas.zygas@gmail.com>
2023-08-01 09:47:21 +0300

    Made navbar render dymnamically based on the url path

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -39,111 +39,103 @@ function Nav() {
     }
   };
 
+  const handleLogin = () => {
+    router.push("/login");
+  };
+
   return (
     <>
       <Toaster />
       <nav className="bg-gray-900 border-gray-200">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <div className=" flex flex-wrap items-center justify-between mx-0 p-4">
           <div className="flex items-center">
             <Image src={Logo} className="h-8 w-5 mr-3" alt="App Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               DEMO
             </span>
           </div>
-          <div className="flex md:order-2">
+
+          {pathname === "/login" || pathname === "/signup" ? (
             <button
               type="button"
               className="text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-0 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0"
-              onClick={handleLogout}
+              onClick={handleLogin}
             >
-              Logout
+              Login
             </button>
-            <button
-              data-collapse-toggle="navbar-sticky"
-              type="button"
-              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-400 rounded-lg md:hidden focus:outline-none focus:ring-2 hover:bg-gray-700 focus:ring-gray-600 transition duration-300 ease-in-out"
-              aria-controls="navbar-sticky"
-              onClick={toggleMenu}
-            >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 17 14"
+          ) : (
+            <>
+              <div className="flex md:order-2">
+                <button
+                  type="button"
+                  className="text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-0 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mx-0"
+                  onClick={handleLogout}
+                >
+                  Logout
+                </button>
+                <button
+                  data-collapse-toggle="navbar-sticky"
+                  type="button"
+                  className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-400 rounded-lg md:hidden focus:outline-none focus:ring-2 hover:bg-gray-700 focus:ring-gray-600 transition duration-300 ease-in-out"
+                  aria-controls="navbar-sticky"
+                  onClick={toggleMenu}
+                >
+                  <span className="sr-only">Open main menu</span>
+                  <svg
+                    className="w-5 h-5"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 17 14"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M1 1h15M1 7h15M1 13h15"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <div
+                className={`items-center justify-between ${
+                  isMenuOpen && "hidden"
+                } w-full md:flex md:w-auto md:order-1 `}
+                id="navbar-cta"
               >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M1 1h15M1 7h15M1 13h15"
-                />
-              </svg>
-            </button>
-          </div>
-          <div
-            className={`items-center justify-between ${
-              isMenuOpen && "hidden"
-            } w-full md:flex md:w-auto md:order-1 `}
-            id="navbar-cta"
-          >
-            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:border-0  bg-gray-800 md:bg-gray-900 border-gray-700">
-              <li>
-                <Link
-                  href="/"
-                  className={`${
-                    pathname === "/"
-                      ? "text-orange-500"
-                      : " hover:underline text-gray-100"
-                  }`}
-                  aria-current="page"
-                >
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/login"
-                  className={`${
-                    pathname === "/login"
-                      ? "text-orange-500"
-                      : " hover:underline text-gray-100"
-                  }`}
-                  aria-current="page"
-                >
-                  Login
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/signup"
-                  className={`${
-                    pathname === "/signup"
-                      ? "text-orange-500"
-                      : " hover:underline text-gray-100"
-                  }`}
-                  aria-current="page"
-                >
-                  Signup
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/profile"
-                  className={`${
-                    pathname === "/profile"
-                      ? "text-orange-500"
-                      : " hover:underline text-gray-100"
-                  }`}
-                  aria-current="page"
-                >
-                  Profile
-                </Link>
-              </li>
-            </ul>
-          </div>
+                <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:border-0  bg-gray-800 md:bg-gray-900 border-gray-700">
+                  <li>
+                    <Link
+                      href="/"
+                      className={`${
+                        pathname === "/"
+                          ? "text-orange-500"
+                          : " hover:underline text-gray-100"
+                      }`}
+                      aria-current="page"
+                    >
+                      Home
+                    </Link>
+                  </li>
+
+                  <li>
+                    <Link
+                      href="/profile"
+                      className={`${
+                        pathname === "/profile"
+                          ? "text-orange-500"
+                          : " hover:underline text-gray-100"
+                      }`}
+                      aria-current="page"
+                    >
+                      Profile
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </>
+          )}
         </div>
       </nav>
     </>
